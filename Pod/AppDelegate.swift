63274e58d392c2b537fc1a432e9be98ab951e445
MC_Doshi <doshi.michael@gmail.com>
2017-05-28 11:42:37 -0700

    Added pod map view

--- Pod/AppDelegate.swift
+++ Pod/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
        // GMSPlacesClient.provideAPIKey(Constants.APIServices.GMSPlacesKey)
-        //GMSServices.provideAPIKey(Constants.APIServices.GMSMapKey)
+        GMSServices.provideAPIKey(Constants.APIServices.GMSMapKey)
         //FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         // Allow Google sign-in
